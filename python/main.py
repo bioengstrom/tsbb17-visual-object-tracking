3a567086aae546f12992c528053e2853ded44fde
Engstrom <isaen688@student.liu.se>
2020-09-17 00:08:40 +0200

    Modify Main to allow: Timing of phases, change learning rate, change image size.

--- python/main.py
+++ python/main.py
@@ -23,7 +23,7 @@ from models.goalNet import goalNet
 from train import train
 from test import test
 
-
+import time #Used to time the training (and test) phase(s) of the network
 
 # Check if CUDA support is available (GPU)
 use_cuda = torch.cuda.is_available()
@@ -33,15 +33,18 @@ use_cuda = torch.cuda.is_available()
 ## Download the CIFAR10 dataset ##
 ##################################
 
+img_size = 32
 
 # Image transformations to apply to all images in the dataset (Data Augmentation)
 transform_train = transforms.Compose([
+    transforms.Resize(img_size),
     transforms.ToTensor(),                # Convert images to Tensors (The data structure that is used by Pytorch)
     transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)), # Normalize the images to zero mean and unit std
 ])
 
 # Image transformations for the test set.
 transform_test = transforms.Compose([
+    transforms.Resize(img_size),
     transforms.ToTensor(),
     transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
 ])
@@ -61,6 +64,9 @@ classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship'
 ## Init the network and optimizer ##
 ####################################
 
+# Learning params
+learning_rate = 0.001 
+
 # Load and initialize the network architecture 
 #model = cvlNet()
 model = goalNet()
@@ -73,19 +79,34 @@ if use_cuda:
 objective = nn.NLLLoss()
 
 # The optimizer used for training the model
-optimizer = torch.optim.Adam(model.parameters()) 
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate) 
 
 
 #######################
 ## Train the network ##
 #######################
+train_time_0 = time.time()
 
 start_epoch = 1
 num_epochs = 50
 model, loss_log, acc_log = train(model, trainloader, optimizer, objective, use_cuda, start_epoch, num_epochs=num_epochs)
 
+train_time_1 = time.time()
 
 ##########################
 ## Evaluate the network ##
 ##########################
+test_time_0 = time.time()
+
 test_acc = test(model, testloader, use_cuda)
+
+test_time_1 = time.time()
+
+#################################
+## Display train and test time ##
+#################################
+print('#############################################')
+print('Epochs: {0}, Learning rate: {1}'.format(num_epochs, learning_rate))
+print('The training time is: {0}s, or {1}min'.format((train_time_1-train_time_0), (train_time_1-train_time_0)/60))
+print('The test time is: {0}'.format(test_time_1-test_time_0))
+print('#############################################')
