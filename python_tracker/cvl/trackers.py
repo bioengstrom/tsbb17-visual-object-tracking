0c9b2851f2f1a054bc4134284068c939c14333d7
filkl010 <filkl010@tlhw-5-3.ad.liu.se>
2020-10-04 13:10:46 +0200

    Plots

--- python_tracker/cvl/trackers.py
+++ python_tracker/cvl/trackers.py
@@ -66,6 +66,8 @@ class NCCTracker:
         return self.boundingBox
 
     def update(self, image, lr=0.1):
+        self.detect(image)
+        
         assert len(image.shape) == 2, "NCC is only defined for grayscale images"
         patch = self.crop_patch(image)
         patch = patch / 255
@@ -73,6 +75,8 @@ class NCCTracker:
         patch = patch / np.std(patch)
         patchf = fft2(patch)
         self.template = self.template * (1 - lr) + patchf * lr
+
+        return copy.deepcopy(self.boundingBox)
 
 class MOSSE_DCF:
     def __init__(self):
@@ -193,3 +197,121 @@ class MOSSE_DCF:
 
         return fft2(patch)
 
+class MOSSE_SCALE:
+    def __init__(self):
+        self.boundingBox = None
+        self.searchRegion = None
+        self.boundingBoxShape = None
+        self.boundingBoxCenter = None
+
+        self.gaussianScore = None
+        self.A = []
+        self.B = 0
+        self.M = []
+        self.P = None
+        self.dims = 1
+
+        self.forgettingFactor = 0.2175
+        self.sigma = 2.0
+        self.regularization = 0.1 # lambda
+
+    #
+    def crop_patch(self, image):
+        #searchRegion = self.searchRegion
+        return crop_patch(image, self.searchRegion)
+
+    def start(self, image, boundingBox, searchRegion):
+        if len(image.shape) > 2:
+            print("colorimage")
+            self.dims = image.shape[2]
+        
+        self.M = [0 for _ in range(self.dims)]
+        self.A = [0 for _ in range(self.dims)]
+        
+        self.searchRegion = searchRegion
+        self.boundingBox = boundingBox
+
+        self.boundingBoxShape = (boundingBox.height, boundingBox.width)
+        self.boundingBoxCenter = (boundingBox.height // 2, boundingBox.width // 2)
+
+        self.searchRegionShape = (searchRegion.height, searchRegion.width)
+        self.searchRegionCenter = (searchRegion.height // 2, searchRegion.width // 2)
+
+        self.gaussianScore = fftGuassianKernel(searchRegion.height, searchRegion.width, self.searchRegionCenter[0], self.searchRegionCenter[1], self.sigma)
+        #plt.imshow(abs(ifft2(self.gaussianScore)))
+        #plt.show()
+
+        # Vi fixar första framen (frame 0), via start
+        self.updateFirstFrame(image)
+
+    def updateFirstFrame(self, image):
+        for dim in range(self.dims):
+            self.P = self.getFFTPatch(image, dim)
+
+            self.A[dim] = self.P * np.conj(self.gaussianScore)
+            self.B += self.P * np.conj(self.P)
+       
+        for dim in range(self.dims):
+            self.M[dim] = self.A[dim] / (self.regularization + self.B)
+
+    def detect(self, image):
+        sum_M = 0
+        for dim in range(self.dims):
+            self.P = self.getFFTPatch(image, dim)
+            # FFT reponse between patch and our learned filter
+            fftresponse = np.conj(self.M[dim]) * self.P    
+            sum_M += fftresponse
+
+
+        response = (ifft2(sum_M))
+
+        # Find maximum response
+        r, c = np.unravel_index(np.argmax(response), response.shape)
+        
+        # Move kernel to new peak
+        self.gaussianScore = fftGuassianKernel(self.searchRegion.height, self.searchRegion.width, r, c, self.sigma)
+        #plt.imshow(abs(ifft2(self.gaussianScore)))
+        #plt.show()
+        r_offset = r - self.searchRegionCenter[0]
+        c_offset = c - self.searchRegionCenter[1]
+
+        # Update pos
+        self.boundingBox.xpos += c_offset
+        self.boundingBox.ypos += r_offset
+
+        self.searchRegion.xpos += c_offset
+        self.searchRegion.ypos += r_offset
+
+        return self.boundingBox
+
+    def update(self, image):
+        self.detect(image) # Detect before update
+
+        B = 0
+        for dim in range(self.dims):
+            # Vi beräknar A
+            self.A[dim] = (self.forgettingFactor * self.P * np.conj(self.gaussianScore)) + (1 - self.forgettingFactor) * self.A[dim]
+           
+            B += self.forgettingFactor * (self.P * np.conj(self.P))
+
+        # Vi beräknar B
+        self.B = B + (1 - self.forgettingFactor) * self.B
+
+        # Vi beräknar M
+        for dim in range(self.dims):
+            self.M[dim] = self.A[dim] / (self.regularization + self.B)
+
+        return copy.deepcopy(self.boundingBox)
+
+    # Returnerar normaliserad FFT av input image (patch)
+    def getFFTPatch(self, image, dim=0):
+        if self.dims > 1:
+            patch = self.crop_patch(image[:, :, dim])
+        else:
+            patch = self.crop_patch(image)
+        
+        patch = patch / 255
+        patch = patch - np.mean(patch)
+        patch = patch / np.std(patch)
+
+        return fft2(patch)
--- python_tracker/example_tracker.py
+++ python_tracker/example_tracker.py
@@ -10,112 +10,156 @@ from cvl.trackers import NCCTracker
 from cvl.trackers import MOSSE_DCF
 from matplotlib import pyplot as plt
 import matplotlib.colors
+import copy
 
 dataset_path = "/courses/TSBB17/otb_mini"
 
 SHOW_BOUNDING_BOX = True
 SHOW_SEARCH_REGION = False
-SEQUENCE_IDXS = [1, 2]
+
+SEQUENCE_IDXS = [6]
+
+TRACKERS = [MOSSE_DCF, NCCTracker]
+Legends = ["MOSSE_DCF", "NCCTracker"] # Used for legends
+
 mode = "COLOR"
 
 if __name__ == "__main__":
 
     dataset = OnlineTrackingBenchmark(dataset_path)
+
+    #per_seq_performance = [ [] for _ in range(len(dataset.sequences))]
+    #seq_performance = [ [] for _ in range(len(dataset.sequences))]
     
-    # For evaluation
-    per_seq_performance = [ [] for _ in range(len(dataset.sequences))]
+    tracker_seq_performance = [ [] for _ in range(len(TRACKERS))]
+    for i in range(len(TRACKERS)):
+        for _ in range(len(dataset.sequences)):
+            tracker_seq_performance[i] = [ [] for _ in range(len(dataset.sequences))]
+
+    
+    per_tracker_performance = [ [] for _ in range(len(TRACKERS))]
 
     for seq_idx in SEQUENCE_IDXS:
-        tracker = MOSSE_DCF()
-        a_seq = dataset[seq_idx]
-        for frame_idx, frame in enumerate(a_seq):
-            print(f"{frame_idx} / {len(a_seq)}", end='\r')
-            image_color = frame['image']
+        bboxStartX = dataset[seq_idx][1]['bounding_box'].xpos
+        bboxStartY = dataset[seq_idx][1]['bounding_box'].ypos
 
-            if mode == "GRAY":
-                image_grayscale = np.sum(image_color, 2) / 3
-                image = image_grayscale
-            elif mode == "COLOR":
-                image = image_color
-            elif mode == "GRADIENTS":
-                image_grayscale = np.sum(image_color, 2) / 3
-                #laplacian = np.expand_dims(cv2.Laplacian(image_grayscale, cv2.CV_64F), 2)
-                sobelx = np.expand_dims(cv2.Sobel(image_grayscale, cv2.CV_64F, 1, 0, ksize=5), 2)
-                sobely = np.expand_dims(cv2.Sobel(image_grayscale, cv2.CV_64F, 0, 1, ksize=5), 2)
-                canny = cv2.Canny(np.uint8(image_grayscale), 100, 300)
-                #plt.imshow(canny, cmap="gray")
-                #plt.imshow(image_grayscale)
-                #plt.show()
-                image = np.concatenate((sobely, sobelx), axis=2)
-                #image = canny
-            elif mode == "HSV":
-                image = matplotlib.colors.rgb_to_hsv(image_color/255)
+        bboxStartW = dataset[seq_idx][1]['bounding_box'].width
+        bboxStartH = dataset[seq_idx][1]['bounding_box'].height
 
+        bboxStart = BoundingBox('tl-size', bboxStartX, bboxStartY, bboxStartW, bboxStartH)
 
-            #searchRegion = None
-            if frame_idx == 0:
-                bbox = frame['bounding_box']
-                if bbox.width % 2 == 0:
-                    bbox.width += 1
-
-                if bbox.height % 2 == 0:
-                    bbox.height += 1
-
-                per_seq_performance[seq_idx].append(bbox)
-
-                # Implementing search region
+        for seqTracker in range(len(TRACKERS)):
+            tracker = TRACKERS[seqTracker]()
+            a_seq = dataset[seq_idx]
+            for frame_idx, frame in enumerate(a_seq):
                 
-                # Changing the parameters individually will grow the search region in a specific direction
-                additionalPixelsX = 50 # Number of additional pixels per side in the x direction
-                additionalPixelsY = 30 # Number of additional pixels per side in the y direction
-                searchRegionPosX = bbox.xpos - additionalPixelsX
-                searchRegionPosY = bbox.ypos - additionalPixelsY
-                searchRegionWidth = bbox.width + 2*additionalPixelsX
-                searchRegionHeight = bbox.height + 2*additionalPixelsY
+                print(f"{frame_idx} / {len(a_seq)}", end='\r')
+                image_color = frame['image']
 
-                searchRegion = BoundingBox('tl-size', searchRegionPosX, searchRegionPosY, searchRegionWidth, searchRegionHeight)
+                if mode == "GRAY" or isinstance(tracker, NCCTracker):
+                    image_grayscale = np.sum(image_color, 2) / 3
+                    image = image_grayscale
+                elif mode == "COLOR":
+                    image = image_color
+                elif mode == "GRADIENTS":
+                    image_grayscale = np.sum(image_color, 2) / 3
+                    #laplacian = np.expand_dims(cv2.Laplacian(image_grayscale, cv2.CV_64F), 2)
+                    sobelx = np.expand_dims(cv2.Sobel(image_grayscale, cv2.CV_64F, 1, 0, ksize=5), 2)
+                    sobely = np.expand_dims(cv2.Sobel(image_grayscale, cv2.CV_64F, 0, 1, ksize=5), 2)
+                    canny = cv2.Canny(np.uint8(image_grayscale), 100, 300)
+                    #plt.imshow(canny, cmap="gray")
+                    #plt.imshow(image_grayscale)
+                    #plt.show()
+                    image = np.concatenate((sobely, sobelx), axis=2)
+                    #image = canny
+                elif mode == "HSV":
+                    image = matplotlib.colors.rgb_to_hsv(image_color/255)
 
-                tracker.start(image, bbox, searchRegion)
-            else:
-                per_seq_performance[seq_idx].append(tracker.update(image))
 
-            if SHOW_BOUNDING_BOX and not SHOW_SEARCH_REGION:
-                bbox = tracker.boundingBox
-                pt0 = (bbox.xpos, bbox.ypos)
-                pt1 = (bbox.xpos + bbox.width, bbox.ypos + bbox.height)
-                image_color = cv2.cvtColor(image_color, cv2.COLOR_RGB2BGR)
-                cv2.rectangle(image_color, pt0, pt1, color=(0, 255, 0), thickness=3)
-                cv2.imshow("Bounding box", image_color)
-                cv2.waitKey(0)
+                #searchRegion = None
+                #print(frame['bounding_box'])
+                if frame_idx == 0:
+                    bbox = copy.deepcopy(bboxStart)
+                    if bbox.width % 2 == 0:
+                        bbox.width += 1
 
-            if SHOW_SEARCH_REGION and not SHOW_BOUNDING_BOX:
-                search_region = tracker.searchRegion
-                pt0search = (searchRegion.xpos, searchRegion.ypos)
-                pt1search = (searchRegion.xpos + searchRegion.width, searchRegion.ypos + searchRegion.height)
-                image_color = cv2.cvtColor(image_color, cv2.COLOR_RGB2BGR)
-                cv2.rectangle(image_color, pt0search, pt1search, color=(0, 0, 255), thickness=3)
-                cv2.imshow("Search region", image_color)
+                    if bbox.height % 2 == 0:
+                        bbox.height += 1
 
-                cv2.waitKey(0)
+                    tracker_seq_performance[seqTracker][seq_idx].append(bbox)
 
-            if SHOW_SEARCH_REGION and SHOW_BOUNDING_BOX:
-                image_color = cv2.cvtColor(image_color, cv2.COLOR_RGB2BGR)
+                    # Implementing search region
+                    
+                    # Changing the parameters individually will grow the search region in a specific direction
+                    if isinstance(tracker, MOSSE_DCF):
+                        additionalPixelsX = 20 # Number of additional pixels per side in the x direction
+                        additionalPixelsY = 20 # Number of additional pixels per side in the y direction
+                        searchRegionPosX = bbox.xpos - additionalPixelsX
+                        searchRegionPosY = bbox.ypos - additionalPixelsY
+                        searchRegionWidth = bbox.width + 2*additionalPixelsX
+                        searchRegionHeight = bbox.height + 2*additionalPixelsY
 
-                bbox = tracker.boundingBox
-                pt0 = (bbox.xpos, bbox.ypos)
-                pt1 = (bbox.xpos + bbox.width, bbox.ypos + bbox.height)
-                cv2.rectangle(image_color, pt0, pt1, color=(0, 255, 0), thickness=3)
-                
-                search_region = tracker.searchRegion
-                pt0search = (searchRegion.xpos, searchRegion.ypos)
-                pt1search = (searchRegion.xpos + searchRegion.width, searchRegion.ypos + searchRegion.height)
-                cv2.rectangle(image_color, pt0search, pt1search, color=(0, 0, 255), thickness=3)
+                        searchRegion = BoundingBox('tl-size', searchRegionPosX, searchRegionPosY, searchRegionWidth, searchRegionHeight)
 
-                cv2.imshow("Search region and bounding box", image_color)
-                cv2.waitKey(0)
+                        tracker.start(image, bbox, searchRegion)
+                    else:
+                        tracker.start(image, bbox)
+                else:
+                    tracker_seq_performance[seqTracker][seq_idx].append(tracker.update(image))
 
-    # Tracking complete, calculate performance
-    per_seq_auc = dataset.calculate_performance(per_seq_performance, SEQUENCE_IDXS)
+                if SHOW_BOUNDING_BOX and not SHOW_SEARCH_REGION:
+                    bbox = tracker.boundingBox
+                    pt0 = (bbox.xpos, bbox.ypos)
+                    pt1 = (bbox.xpos + bbox.width, bbox.ypos + bbox.height)
+                    image_color = cv2.cvtColor(image_color, cv2.COLOR_RGB2BGR)
+                    cv2.rectangle(image_color, pt0, pt1, color=(0, 255, 0), thickness=3)
+                    cv2.imshow("Bounding box", image_color)
+                    cv2.waitKey(20)
+
+                elif SHOW_SEARCH_REGION and not SHOW_BOUNDING_BOX:
+                    search_region = tracker.searchRegion
+                    pt0search = (searchRegion.xpos, searchRegion.ypos)
+                    pt1search = (searchRegion.xpos + searchRegion.width, searchRegion.ypos + searchRegion.height)
+                    image_color = cv2.cvtColor(image_color, cv2.COLOR_RGB2BGR)
+                    cv2.rectangle(image_color, pt0search, pt1search, color=(0, 0, 255), thickness=3)
+                    cv2.imshow("Search region", image_color)
+
+                    cv2.waitKey(20)
+
+                elif SHOW_SEARCH_REGION and SHOW_BOUNDING_BOX:
+                    image_color = cv2.cvtColor(image_color, cv2.COLOR_RGB2BGR)
+
+                    bbox = tracker.boundingBox
+                    pt0 = (bbox.xpos, bbox.ypos)
+                    pt1 = (bbox.xpos + bbox.width, bbox.ypos + bbox.height)
+                    cv2.rectangle(image_color, pt0, pt1, color=(0, 255, 0), thickness=3)
+                    
+                    search_region = tracker.searchRegion
+                    pt0search = (searchRegion.xpos, searchRegion.ypos)
+                    pt1search = (searchRegion.xpos + searchRegion.width, searchRegion.ypos + searchRegion.height)
+                    cv2.rectangle(image_color, pt0search, pt1search, color=(0, 0, 255), thickness=3)
+
+                    cv2.imshow("Search region and bounding box", image_color)
+                    cv2.waitKey(20)
+
+            # Tracking complete, calculate performance
+            per_tracker_performance[seqTracker] = dataset.calculate_performance(tracker_seq_performance[seqTracker], SEQUENCE_IDXS)
+
+        #print(per_tracker_performance)
+        #print(per_tracker_performance[0])
+        print("\n")
+        for t in range(len(per_tracker_performance)):
+            for s in range(len(SEQUENCE_IDXS)):
+                plt.plot(per_tracker_performance[t][s], label=Legends[t])
+                plt.legend()
+        
+        plt.show()
+        #plt.plot(per_tracker_performance[0])
+        #for tracker in per_tracker_performance:
+            #print(tracker)
+            #plt.plot(tracker)
+
+        #plt.show()
 
 
 
