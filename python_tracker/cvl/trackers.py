0a7ea2151ce48bad40c73593a8ba8261c5f854ec
dylma900 <dylma900@student.liu.se>
2020-10-05 17:15:26 +0200

    Add MOSSE_DEEP, and hanning to MOSSE_DCF

--- python_tracker/cvl/trackers.py
+++ python_tracker/cvl/trackers.py
@@ -9,6 +9,17 @@ import cv2
 import matplotlib.pyplot as plt
 import copy
 
+from cvl.features import alexnetFeatures
+import torch
+from PIL import Image
+from torchvision import transforms
+
+def hanning(patch):
+    hanning_w = np.hanning(patch.shape[0])
+    hanning_h = np.hanning(patch.shape[1])
+    hanning_2d = np.sqrt(np.outer(hanning_w, hanning_h))
+    return patch*hanning_2d
+
 # kernel becomes kernelLength by kernelLength
 def fftGuassianKernel(kernelHeight, kernelWidth, peakRow, peakColumn, sigma=2.0): 
     x = np.zeros([kernelHeight, kernelWidth])
@@ -189,10 +200,11 @@ class MOSSE_DCF:
             patch = self.crop_patch(image[:, :, dim])
         else:
             patch = self.crop_patch(image)
-        
+
         patch = patch / 255
         patch = patch - np.mean(patch)
         patch = patch / np.std(patch)
+        patch = hanning(patch)
 
         return fft2(patch)
 
@@ -314,3 +326,150 @@ class MOSSE_SCALE:
         patch = patch / np.std(patch)
 
         return fft2(patch)
+
+class MOSSE_DEEP:
+    def __init__(self):
+        self.model = alexnetFeatures(pretrained=True)
+        self.first_layer = self.model.features[0:2]
+
+        self.boundingBox = None
+        self.searchRegion = None
+        self.boundingBoxShape = None
+        self.boundingBoxCenter = None
+
+        self.gaussianScore = None
+        self.A = []
+        self.B = 0
+        self.M = []
+        self.P = None
+        self.dims = 64
+
+        self.forgettingFactor = 0.2175
+        self.sigma = 2.0
+        self.regularization = 0.1 # lambda
+
+        # https://pytorch.org/hub/pytorch_vision_alexnet/
+        self.preprocess = transforms.Compose([
+                transforms.Resize(256),
+                transforms.CenterCrop(224),
+                transforms.ToTensor(),
+                transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+        ])
+
+
+    #
+    def crop_patch(self, image):
+        #searchRegion = self.searchRegion
+        return crop_patch(image, self.searchRegion)
+
+    
+
+    def start(self, image, boundingBox, searchRegion): 
+        self.M = [0 for _ in range(self.dims)]
+        self.A = [0 for _ in range(self.dims)]
+        
+        self.searchRegion = searchRegion
+        self.boundingBox = boundingBox
+
+        self.boundingBoxShape = (boundingBox.height, boundingBox.width)
+        self.boundingBoxCenter = (boundingBox.height // 2, boundingBox.width // 2)
+
+        self.searchRegionShape = (searchRegion.height, searchRegion.width)
+        self.searchRegionCenter = (searchRegion.height // 2, searchRegion.width // 2)
+
+        self.gaussianScore = fftGuassianKernel(searchRegion.height, searchRegion.width, self.searchRegionCenter[0], self.searchRegionCenter[1], self.sigma)
+        #plt.imshow(abs(ifft2(self.gaussianScore)))
+        #plt.show()
+
+        # Vi fixar första framen (frame 0), via start
+        self.updateFirstFrame(image)
+
+    def updateFirstFrame(self, image):
+        patches = self.getPatchesDeep(image)
+        for dim in range(self.dims):
+            patch = patches[dim]
+            patch = self.normalize(patch)
+            patch = hanning(patch)
+            self.P = fft2(patch)
+            self.A[dim] = self.P * np.conj(self.gaussianScore)
+            self.B += self.P * np.conj(self.P)
+       
+        for dim in range(self.dims):
+            self.M[dim] = self.A[dim] / (self.regularization + self.B)
+
+    def detect(self, image):
+        sum_M = 0
+        patches = self.getPatchesDeep(image)
+        for dim in range(self.dims):
+            patch = patches[dim]
+            patch = self.normalize(patch)
+            patch = hanning(patch)
+            self.P = fft2(patch)
+            # FFT reponse between patch and our learned filter
+            fftresponse = np.conj(self.M[dim]) * self.P    
+            sum_M += fftresponse
+
+
+        response = (ifft2(sum_M))
+
+        # Find maximum response
+        r, c = np.unravel_index(np.argmax(response), response.shape)
+        
+        # Move kernel to new peak
+        self.gaussianScore = fftGuassianKernel(self.searchRegion.height, self.searchRegion.width, r, c, self.sigma)
+        #plt.imshow(abs(ifft2(self.gaussianScore)))
+        #plt.show()
+        r_offset = r - self.searchRegionCenter[0]
+        c_offset = c - self.searchRegionCenter[1]
+
+        # Update pos
+        self.boundingBox.xpos += c_offset
+        self.boundingBox.ypos += r_offset
+
+        self.searchRegion.xpos += c_offset
+        self.searchRegion.ypos += r_offset
+
+        return self.boundingBox
+
+    def update(self, image):
+        self.detect(image) # Detect before update
+
+        B = 0
+        for dim in range(self.dims):
+            # Vi beräknar A
+            self.A[dim] = (self.forgettingFactor * self.P * np.conj(self.gaussianScore)) + (1 - self.forgettingFactor) * self.A[dim]
+           
+            B += self.forgettingFactor * (self.P * np.conj(self.P))
+
+        # Vi beräknar B
+        self.B = B + (1 - self.forgettingFactor) * self.B
+
+        # Vi beräknar M
+        for dim in range(self.dims):
+            self.M[dim] = self.A[dim] / (self.regularization + self.B)
+
+        return copy.deepcopy(self.boundingBox)
+
+    def normalize(self, patch):
+        # patch = patch / 255
+        patch = patch - np.mean(patch)
+        patch = patch / np.std(patch)
+        return patch
+
+    def getPatchesDeep(self, image):
+        
+        r = self.crop_patch(image[:, :, 0])
+        g = self.crop_patch(image[:, :, 1])
+        b = self.crop_patch(image[:, :, 2])
+        
+        rgb_patch = np.dstack((r,g,b))
+        PIL_patch = Image.fromarray(rgb_patch)
+        input_tensor = self.preprocess(PIL_patch)
+        input_batch = input_tensor.unsqueeze(0) # create a mini-batch as expected by the model
+        res = self.first_layer(input_batch)
+        res = torch.nn.functional.interpolate(res, self.searchRegionShape) # resize images to searchRegion
+        res = res.detach().numpy()
+        res = res.squeeze() #remove first uneccesary dim
+        #res = np.moveaxis(res, 0, -1) # (dims, w, h) => (w, h, dims)
+
+        return res
